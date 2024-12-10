707f3243d3e9e718f5461b586114563d04461554
rmitnl <rmartin@absoluteingenuity.net>
2024-12-10 10:01:27 -0500

    test

--- get_int_counters/get_int_counters.py
+++ get_int_counters/get_int_counters.py
@@ -33,7 +33,6 @@ if interface not in ["ethernet 1/1/1", "ethernet 1/1/2", "ethernet 1/1/3", "ethe
     sys.exit(1)
 
 # run the show command
-print('Hello Itential!!!')
 print('--------------------------------')
 print(device,interface)
 print('--------------------------------')
