5914283dfc9cbb618645aafc0fbbb15f0e4cd27b
rmitnl <rmartin@absoluteingenuity.net>
2024-12-17 11:15:35 -0500

    test

--- get_int_counters/get_int_counters.py
+++ get_int_counters/get_int_counters.py
@@ -33,7 +33,7 @@ if interface not in ["ethernet 1/1/1", "ethernet 1/1/2", "ethernet 1/1/3", "ethe
     sys.exit(1)
 
 # run the show command
-print('Version 1.0')
+print('Version 2.0')
 print('--------------------------------')
 print(device,interface)
 print('--------------------------------')
