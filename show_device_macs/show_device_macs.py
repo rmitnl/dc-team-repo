b2f2718b27cc722fce38a78388aaeedb03c57d24
rmitnl <rmartin@absoluteingenuity.net>
2025-05-21 10:18:31 -0400

    update script

--- show_device_macs/show_device_macs.py
+++ show_device_macs/show_device_macs.py
@@ -4,12 +4,12 @@ from netmiko import ConnectHandler
 # run the show command
 network = ConnectHandler(device_type='brocade_fastiron',ip="192.168.123.12",username=var_u,password=var_p,secret=var_s)
 output = network.send_command("show mac-address")
-print(output)
-#for macs in output.splitlines():
-#    tokens = macs.split()
-#    if len(tokens) > 0:
-#        if tokens[0] not in ["MAC-Address","Total"]:
-#            print(tokens[0],tokens[1],tokens[4])
+#print(output)
+for macs in output.splitlines():
+    tokens = macs.split()
+    if len(tokens) > 0:
+        if tokens[0] not in ["MAC-Address","Total"]:
+            print(tokens[0],tokens[1],tokens[4])
 print('--------------------------------')
 print('Version 1.0')
 print('--------------------------------')
